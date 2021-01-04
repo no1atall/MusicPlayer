9aeb11c312666550d5ef459bd4af50a0e375fbca
Ian V <ianenrico1@gmail.com>
2021-01-03 23:14:55 -0500

    Util file. Initial song data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+
 //Import Styles
 import "./styles/app.scss";
 
 //Import Components
 import Player from "./components/Player";
 import Song from "./components/Song";
+
+//Import Util
+import data from "./util";
 
 function App() {
   return (
